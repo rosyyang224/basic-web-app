194b6a60b0e98471a272a8c445b2aaabb3c92647
Rosemary Yang <rey@andrew.cmu.edu>
2024-10-10 10:19:31 -0400

    added a query to add two numbers along with tests

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -25,4 +25,9 @@ describe("QueryProcessor", () => {
             "rey"
           ));
     });
+    
+    test('should add numbers', () => {
+        expect(QueryProcessor("What is 5 plus 52?")).toEqual("57");
+        expect(QueryProcessor("What is 12 plus 1?")).toEqual("13");
+    });
 });
